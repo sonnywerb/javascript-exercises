83d1ef52a39219bf472321196c881c45c029d7fc
Eric Chan <ericchan024@gmail.com>
2022-08-25 22:37:41 -0400

    completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    // create a variable to hold the string
+    // loop n times and on each iteration we append the string to the variable
+    // return the string
+    if (n < 0) {
+        return 'ERROR'
+    }
+    let result = '';
+    let i = 1;
+    while (i <= n) {
+        result += `${string}`;
+        i++;
+    }
+    return result;
 };
 
 // Do not edit below this line
