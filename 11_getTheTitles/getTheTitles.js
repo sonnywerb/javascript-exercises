ed2921d8caf3760296b23e8120faa7ee083492f4
Eric Chan <ericchan024@gmail.com>
2022-09-27 19:09:57 -0400

    finished getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles = books.map(book => `${book.title}`);
+    return titles;
 };
+// const arr = [
+//     {
+//       title: 'Book',
+//       author: 'Name'
+//     },
+//     {
+//       title: 'Book2',
+//       author: 'Name2'
+//     }
+//   ]
+// console.log(getTheTitles(arr));
 
 // Do not edit below this line
 module.exports = getTheTitles;
