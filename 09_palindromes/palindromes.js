47767a549750510c9c98f3d43038563b6359492b
Eric Chan <ericchan024@gmail.com>
2022-09-27 18:21:36 -0400

    finished 09_palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,36 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let start = 0;
+    let end = str.length - 1;
+    str = str.toUpperCase();
 
+    while (start <= end) {
+        let startLetter = str.charAt(start);
+        let endLetter = str.charAt(end);
+        while (!isLetter(startLetter)) {
+            start++;
+            startLetter = str.charAt(start);
+        }
+        while (!isLetter(endLetter)) {
+            end--;
+            endLetter = str.charAt(end);
+        }
+        if (str.charAt(start) !== str.charAt(end)) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+
+    return true;
 };
+
+function isLetter(char) {
+    if (char.match(/[a-zA-Z]/i)) {
+        return true;
+    }
+}
+
+console.log(palindromes('Racecar!'));
 
 // Do not edit below this line
 module.exports = palindromes;
