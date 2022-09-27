74fdb31dff19e395fc8b435d96b9a1c7a56ba12b
Eric Chan <ericchan024@gmail.com>
2022-09-27 18:29:27 -0400

    finished 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 0) return 'OOPS';
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    return fibonacci(n-1) + fibonacci(n-2);
 };
+
+// console.log(fibonacci('1'));
 
 // Do not edit below this line
 module.exports = fibonacci;
